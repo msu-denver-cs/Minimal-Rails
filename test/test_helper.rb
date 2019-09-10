04f0a1d4fba6ef82d5bd3efb4a9c16f535d7a655
drsjb80 <drsjb80@gmail.com>
2019-09-10 08:47:42 -0600

    Getting SimpleFormatter to work so the results can be seen on Travis

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,16 @@
 require 'simplecov'
-SimpleCov.start
+SimpleCov.formatter = SimpleCov::Formatter::SimpleFormatter
+
+# Have to have this even though it's supposed to be the default
+SimpleCov.at_exit do
+  puts SimpleCov.result.format!
+end
+
+# Have to have these even though they're not required for the default formatter.
+SimpleCov.start do
+  add_group "app", "app"
+  add_group "test", "test"
+end
 
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
